2e7cba4a8e1de0586d1462a5a4647331162e3797
guohaozhao116008 <guohaozhao116008@sohu-inc.com>
2015-05-16 16:14:45 +0800

    增加第十章例子

---
+++ beginning_html_and_css/ch10/js/practice.js
@@ -0,0 +1,14 @@
+/**
+ * Created by babyduncan on 15/5/16.
+ */
+
+
+var string_ = "Learn javascript build-in objects is so easy !";
+
+var myString = string_.substring(0, 10);
+
+myString = myString.toUpperCase();
+
+document.body.innerHTML += "<h1>" + myString + "</h1>";
+
+document.body.innerHTML += "<h1>" + window.CSSValue + "</h1>"
